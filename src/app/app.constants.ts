d8f6ab9c28ec0a5f71ba843d78ae097136322a6c
jariwbh <jariwbh@gmail.com>
2017-07-22 17:18:08 +0530

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -6,7 +6,7 @@ export class Configuration {
     public Server: string;
 
     constructor() {
-       if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
+       if (location.hostname === 'localhost' || location.hostname === '127.0.0.1' || localhost.hostname === '52.163.113.185') {
             this.Server = 'http://localhost:4200/';
        } else {
             this.Server = 'http://www.almuadin.net/';
