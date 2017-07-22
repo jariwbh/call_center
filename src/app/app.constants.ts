7140593f600a86c5e9df28391487ac69496d2c5e
jariwbh <jariwbh@gmail.com>
2017-07-22 17:31:18 +0530

    Update app.constants.ts

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -6,9 +6,12 @@ export class Configuration {
     public Server: string;
 
     constructor() {
-       if (location.hostname === 'localhost' || location.hostname === '127.0.0.1' || location.hostname === '52.163.113.185') {
+       if (location.hostname === 'localhost' || location.hostname === '127.0.0.1' ) {
             this.Server = 'http://localhost:4200/';
-       } else {
+       } else if (location.hostname === '52.163.113.185') { 
+            this.Server = 'http://52.163.113.185/';
+       }
+      else {
             this.Server = 'http://www.almuadin.net/';
        }
     }
