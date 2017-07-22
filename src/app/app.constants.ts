5b9e824a99ff06634b6f566964285e4c802c48c0
jariwbh@krtya.com <jariwbh@gmail.com>
2017-07-22 17:50:09 +0530

    api updated

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -7,12 +7,11 @@ export class Configuration {
 
     constructor() { 
       
-            if (location.hostname === 'localhost' || location.hostname === '127.0.0.1' ) {
+      if (location.hostname === 'localhost' || location.hostname === '127.0.0.1' ) {
             this.Server = 'http://localhost:4200/';
       } else if (location.hostname === '52.163.113.185') { 
             this.Server = 'http://52.163.113.185:3000/';
-       }
-      else {
+      } else {
             this.Server = 'http://www.almuadin.net/';
       }
     }
